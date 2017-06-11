ede73ae1f216fd0162a9fc8abc09e953c87b7729
Efe Ariaroo <eariaroo@c2gconsulting.com>
2017-06-11 17:30:15 +0100

    Got report service to register itself so that it is discoverable

---
+++ report/config/development/env-template.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+
+# cluster env vars
+export CLUSTER_DISCOVERY_URL="mongodb://localhost/service-discovery"
+export CLUSTER_SERVICE="report"
+export CLUSTER_PUBLIC_SERVICES="report"
+
+# mongodb urls
+export MONGO_URL="mongodb://localhost:27017/meteor"
+export MONGO_OPLOG_URL="mongodb://localhost:27017/meteor"
+export DATA_MONGO_URL="mongodb://localhost:27017/meteor"
